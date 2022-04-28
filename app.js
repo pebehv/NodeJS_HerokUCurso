e98c344341cde3d27bfb0d397ddadcc045884342
Practicando <pebehv@gmail.com>
2022-04-28 14:30:20 -0400

    10mo finalizando curso

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 
 //const uri = 'mongodb+srv://nodejs_yt:xSVhWhk7bRYv5GMI@cluster0.dfmua.mongodb.net/veterianaria?retryWrites=true&w=majority';
 //const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.dfmua.mongodb.net/${process.env.NAMEBD}?retryWrites=true&w=majority`;
-
+//const uri = process.env.URI
 mongoose.connect(process.env.URI, ()=>{
    // useNewUrlParser: true
     console.log("uri", process.env.URI);
